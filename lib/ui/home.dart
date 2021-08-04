a4f1d94199047362ca3db9e39486a150954938c3
eceCaliskan <ece98caliskan@gmail.com>
2021-08-04 15:33:52 +0300

    adding bottom navigation bar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,107 +1,62 @@
-import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:flutter/material.dart';
-import 'package:socialmediapp/ui/welcome.dart';
-
-import 'login.dart';
 
 
 
+/// This is the stateful widget that the main application instantiates.
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
-  _Home createState() =>
-      _Home();
+  State<Home> createState() => _Home();
 }
 
-class _Home
-    extends State<Home> {
-  final BorderRadius _borderRadius = const BorderRadius.only(
-    topLeft: Radius.circular(25),
-    topRight: Radius.circular(25),
-  );
+/// This is the private State class that goes with MyStatefulWidget.
+class _Home extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static  List<Widget> _widgetOptions = <Widget>[
+    Container(color: Colors.red),
+    Container(color: Colors.pink),
+    Container(color: Colors.yellow),
+    Container(color: Colors.red),
 
-  ShapeBorder? bottomBarShape = const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(25)),
-  );
-  SnakeBarBehaviour snakeBarStyle = SnakeBarBehaviour.floating;
-  EdgeInsets padding = const EdgeInsets.all(12);
-
-  int _selectedItemPosition = 2;
-  SnakeShape snakeShape = SnakeShape.circle;
-
-  bool showSelectedLabels = false;
-  bool showUnselectedLabels = false;
-
-  Color selectedColor = Colors.black;
-  Gradient selectedGradient =
-  const LinearGradient(colors: [Colors.red, Colors.amber]);
-
-  Color unselectedColor = Colors.blueGrey;
-  Gradient unselectedGradient =
-  const LinearGradient(colors: [Colors.red, Colors.blueGrey]);
-
-  Color? containerColor;
-  List<Color> containerColors = [
-    const Color(0xFFFDE1D7),
-    const Color(0xFFE4EDF5),
-    const Color(0xFFE7EEED),
-    const Color(0xFFF4E4CE),
   ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      resizeToAvoidBottomInset: true,
-      extendBody: true,
       appBar: AppBar(
-        centerTitle: false,
-        brightness: Brightness.light,
-        leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () {}),
-        title: const Text('Go back', style: TextStyle(color: Colors.black)),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
+        title: const Text('BottomNavigationBar Sample'),
       ),
-      body: AnimatedContainer(
-        color: containerColor ?? containerColors[0],
-        duration: const Duration(seconds: 1),
-        child: PageView(
-          //onPageChanged: _onPageChanged,
-          children: <Widget>[
-Login(),
-            Welcome(),
-          ],
-        ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: SnakeNavigationBar.color(
-        // height: 80,
-        behaviour: snakeBarStyle,
-        snakeShape: snakeShape,
-        shape: bottomBarShape,
-        padding: padding,
-
-        ///configuration for SnakeNavigationBar.color
-        snakeViewColor: selectedColor,
-        selectedItemColor:
-        snakeShape == SnakeShape.indicator ? selectedColor : null,
-        unselectedItemColor: Colors.blueGrey,
-
-
-        showUnselectedLabels: showUnselectedLabels,
-        showSelectedLabels: showSelectedLabels,
-
-        currentIndex: _selectedItemPosition,
-        onTap: (index) => setState(() => _selectedItemPosition = index),
-        items: [
-          const BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'tickets'),
-          const BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: 'calendar'),
-
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
         ],
-        selectedLabelStyle: const TextStyle(fontSize: 14),
-        unselectedLabelStyle: const TextStyle(fontSize: 10),
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
-  }}
+  }
+}
