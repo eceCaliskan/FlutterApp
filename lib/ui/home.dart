1d7b9fd92dc6fd971542880664d7852d36f3a552
eceCaliskan <ece98caliskan@gmail.com>
2021-08-04 16:03:14 +0300

    changes on the bottom navigation bar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,60 +1,60 @@
 import 'package:flutter/material.dart';
 
-
-
 /// This is the stateful widget that the main application instantiates.
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _Home();
 }
-
 /// This is the private State class that goes with MyStatefulWidget.
 class _Home extends State<Home> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static  List<Widget> _widgetOptions = <Widget>[
-    Container(color: Colors.red),
-    Container(color: Colors.pink),
-    Container(color: Colors.yellow),
-    Container(color: Colors.red),
+     int _selectedIndex = 0;
+     static const TextStyle optionStyle =
+     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
-  ];
+     static List<Widget> _widgetOptions = <Widget>[
+          Container(color: Colors.red),
+          Container(color: Colors.pink),
+          Container(color: Colors.yellow),
+          Container(color: Colors.red),
+     ];
 
-  void _onItemTapped(int index) {
+    void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
+        backgroundColor: Colors.white,
+        title: Center(child: const Text('Connect', style: TextStyle(color: Colors.orange),)),
+        iconTheme: IconThemeData(color: Colors.black),
+
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
+            BottomNavigationBarItem(
+               icon: Icon(Icons.home),
+               label: 'Home',
+            ),
+            BottomNavigationBarItem(
+               icon: Icon(Icons.post_add),
+               label: 'Post',
+            ),
+            BottomNavigationBarItem(
+               icon: Icon(Icons.message),
+               label: 'Message',
+            ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black54,
         onTap: _onItemTapped,
       ),
     );
