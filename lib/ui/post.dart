a16f39f1468706fced0f367049acbd05a74402bd
eceCaliskan <ece98caliskan@gmail.com>
2021-08-14 20:09:34 +0300

    updates

--- lib/ui/post.dart
+++ lib/ui/post.dart
@@ -1,48 +1,75 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialmediapp/bloc/post/post_bloc.dart';
+import 'package:socialmediapp/bloc/post/post_state.dart';
+import 'package:socialmediapp/services/database.dart';
 
-class Post extends StatelessWidget {
+import 'home.dart';
+
+class Post extends StatefulWidget{
+  @override
+
+  const Post({Key? key}) : super(key: key);
+
+  @override
+  _Post createState() => _Post();
+}
+
+
+class _Post extends State<Post> {
+  UserRepository _userRepository = UserRepository();
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    PostBloc _postBloc = new PostBloc(userRepository: _userRepository);
 
-      body: ListView(
-        children: <Widget>[
-          Container(height: MediaQuery
-          .of(context)
-          .size
-          .width / 0.9
+    return BlocBuilder(
+        bloc: _postBloc,
+        builder: (BuildContext context, state) {
+          if (state is AdddedToTheDatabase) {
+            //BlocProvider.of<AuthenticationBloc>(context).add(Auth());
+            Future.delayed(const Duration(milliseconds: 500), () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) => Home()));
+            }
+            );
+          }
 
-            ,width: 1000, color: Colors.pink,
-          child: Text("hellsdkgşkfsgkşfgkşfdkggdfhnfghrfg"),
-
-          ),
-
-
-
-        Container(
-              color: Colors.white,
-              child: TextField(
-                minLines: 1,
-                 maxLines: 10,
-                 decoration: InputDecoration(
-                   border: OutlineInputBorder( borderSide:  BorderSide(color: Colors.white, width: 2.0),
-               ),
-                   hoverColor: Colors.orange,
-
-                   hintText: "Enter your post",
-
-
-
-                 ),
-              ),
+          return Scaffold(
+            body: ListView(
+              children: <Widget>[
+                Container(
+                  color: Colors.white,
+                  child: TextField(
+                    minLines: 1,
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                      hoverColor: Colors.orange,
+                      hintText: "Enter your post",
+                    ),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .width / 0.9,
+                  //width: 1000,
+                  color: Colors.pink,
+                  child: Text("hellsdkgşkfsgkşfgkşfdkggdfhnfghrfg"),
+                ),
+              ],
             ),
-        ],
-          ),
 
-
-
+          );
+        }
     );
+
+
   }
 }
